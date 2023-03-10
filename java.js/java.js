b280a739b85de95f46343dcf4f305c7cb41dece1
ANDYZ29 <127368479+ANDYZ29@users.noreply.github.com>
2023-03-10 00:51:58 -0300

    Update java.js

--- java.js/java.js
+++ java.js/java.js
@@ -1,6 +1,6 @@
-var input = document.getElementsByClassName('formulario_input');
+var inputs = document.getElementsByClassName('formulario_input');
 for (var i = 0 ; i < inputs.length; i++) {
-    input[i].addEventListener("keyup", function(){
+    inputs[i].addEventListener("keyup", function(){
         if(this.value.length>=1){
         this.nextElementSibling.classList.add("fijar");
     } else {
